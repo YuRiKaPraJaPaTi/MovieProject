f8cd04b4190759eb6899f270198b5cb762ed073b
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2025-08-04 13:46:41 +0545

    WelcomeScreen add

--- App.tsx
+++ App.tsx
@@ -5,16 +5,15 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+
+import { StyleSheet, View } from 'react-native';
+import WelcomeScreen from './src/Auth/WelcomeScreen';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <WelcomeScreen />
     </View>
   );
 }
---
+++ src/Auth/WelcomeScreen.tsx
@@ -0,0 +1,77 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const WelcomeScreen = () => {
+  return (
+    <View style={styles.container}>
+            <Image 
+                  source={require('../assets/Banner.png')}
+                  style={styles.banner}
+            />
+            <Image 
+                  source={require('../assets/LOGO.png')}
+                  style={styles.logo}
+            />
+
+            <View style={styles.textContainer}>
+                  <Text style={styles.text}>"Track films you've watched. Save these you want to see."</Text>
+            </View>
+
+            <TouchableOpacity style={styles.getStarted}>
+                  <Text style={styles.buttonText}>Get Started</Text>
+            </TouchableOpacity>
+      
+    </View>
+  )
+}
+
+export default WelcomeScreen
+
+const styles = StyleSheet.create({
+      container: {
+            flex:1,
+            backgroundColor: '#002335'
+      },
+      banner: {
+            width: "100%",
+            position: 'absolute',
+      },
+      logo: {
+            width:158,
+            height: 189,
+            position: 'relative',
+            top: 236,
+            alignSelf: 'center',
+      },
+      textContainer: {
+            padding: 30,
+            // backgroundColor: 'red',
+            alignItems: 'center',
+            justifyContent:'center',
+            marginTop: 300,
+      },
+      text: {
+            fontSize: 32,
+            fontFamily: 'Poppins',
+            fontWeight: 700,
+            color: 'white',
+            textAlign: 'center',
+      },
+      getStarted: {
+            backgroundColor: '#FFCA45',
+            // padding: 40,
+            width: 200,
+            height: 56,
+            alignSelf: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 16,
+      },
+      buttonText: {
+            fontSize: 24,
+            alignSelf: 'center',
+            color: 'white',
+            
+      }
+      
+})
