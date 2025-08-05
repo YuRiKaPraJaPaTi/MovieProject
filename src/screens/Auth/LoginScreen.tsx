d67c15ad6ff431f638e50379c093ccc8fd399804
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2025-08-05 13:11:13 +0545

    chnges in loginscreen ie using container

--- src/screens/Auth/LoginScreen.tsx
+++ src/screens/Auth/LoginScreen.tsx
@@ -6,6 +6,7 @@ import MyButton from '../../components/MyButton';
 import InputBox from '../../components/InputBox';
 import { BlurView } from '@react-native-community/blur';
 import GoogleButton from '../../components/GoogleButton';
+import LoginSignupContainer from '../../components/LoginSignupContainer';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Login'>;
 
@@ -16,32 +17,12 @@ const LoginScreen = ({navigation}:Props) => {
 
             <Image source={require('../../assets/LOGOcinephiler.png')} />
 
-            <View style={styles.loginContainer}>
-
-                  <Text style={styles.title}>Login</Text>
-
-                  <Text style={styles.text1}>Please signin to continue</Text>
-
-                  <InputBox iconName={require('../../assets/Username.png')} placeholder='Username'/>
-                  <InputBox iconName={require('../../assets/Password.png')} placeholder='Password'/>
-
-                  <Text style={styles.forgotPass}>Forgot Password??</Text>
-
-                  <MyButton label='Login' onPress={()=>{navigation.navigate('Signup')}} />
-                  
-                  <View style={styles.foot}>
-                        <Text style={styles.normalText}>Don't have an account?</Text>
-                        <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
-                        <Text style={styles.signupLink}> SignUp </Text>
-                        </TouchableOpacity>
-                        <Text style={styles.normalText}>first</Text>
-                  </View>
-
-                  <Text style={{fontSize:16, marginVertical:10, color:'white'}}>OR</Text>
-
-                  <GoogleButton label='Google' iconName='google'/>
-                  
-            </View>
+            <LoginSignupContainer 
+                  title='LogIn' 
+                  buttonLabel='LogIn' 
+                  onPressButton={()=>navigation.navigate('Signup')} 
+                  onToggleForm={()=>navigation.navigate('Signup')}
+            />
 
       </ImageBackground>
       
@@ -51,49 +32,5 @@ const LoginScreen = ({navigation}:Props) => {
 export default LoginScreen
 
 const styles = StyleSheet.create({
-      loginContainer: {
-            position: 'relative', 
-            top:250, 
-            padding: 20,
-            backgroundColor: 'rgba(255, 255, 255, 0.7)', 
-            // backgroundColor: 'gray',
-            borderRadius: 20,
-            justifyContent: 'center',
-            alignItems: 'center',
-            
-            
-      },
-      title: {
-            fontSize: 44,
-            fontWeight: 800,
-            color: 'white',
-      },
-      text1: {
-            fontSize: 16,
-            fontWeight: 600,
-            color: 'white',
-            marginBottom: 12,
-      },
-      forgotPass: {
-            fontSize: 16,
-            alignSelf: 'flex-end',
-            color: '#FFCA45',
-            marginBottom: 12,
-      },
-      foot: {
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 20,
-      },
-      normalText: {
-            fontSize: 16,
-            color: 'white', 
-      },
-      signupLink: {
-            fontSize: 16,
-            color: '#FFCA45', 
-            fontWeight: 600,
-      },
+      
 })
