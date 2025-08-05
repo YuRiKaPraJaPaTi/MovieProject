80c1e3ab03dc92714ebf05f4c87880b80041bcb4
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2025-08-05 17:06:22 +0545

    signout added in profile screen

--- src/screens/BottomTabScreens/ProfileScreen.tsx
+++ src/screens/BottomTabScreens/ProfileScreen.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import auth, { getAuth, signOut } from '@react-native-firebase/auth';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const ProfileScreen = () => {
-  return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
-  )
+      const user = auth().currentUser;
+      const email = user?.email ?? 'No Email';
+      const initial = email.charAt(0).toUpperCase();
+
+      const handleSignOut = async () => {
+            const auth = getAuth();
+            try {
+                  await signOut(auth);
+                  Alert.alert("Signed out", "You have been signed out successfully.");
+                  
+            } catch (error) {
+                  Alert.alert("Error", "Something went wrong while signing out.");
+                  console.error("Sign-out error:", error);
+            }
+      };
+      return (
+      
+            <TouchableOpacity style={styles.logout} onPress={handleSignOut}>
+                  <Icon name="sign-out" size={40} color="#FFCA45" />
+                  <Text style={{color:'#FFCA45'}}>LogOut</Text>
+            </TouchableOpacity>
+      
+      )
 }
 
 export default ProfileScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+      logout: {
+            marginRight: 20,
+            alignItems: 'flex-end',
+            paddingVertical: 15,
+            borderBottomColor: '#eee',
+            borderBottomWidth: 1,
+            // backgroundColor: 'black'
+      },
+})
