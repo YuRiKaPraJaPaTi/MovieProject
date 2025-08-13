81fcdcf88466aa0bf89a308333dbd3c11df6fa79
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2025-08-13 06:15:53 +0545

    success,error,info toast

--- App.tsx
+++ App.tsx
@@ -6,19 +6,20 @@
  */
 
 
-import { StatusBar, StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
 import { store } from './src/redux/store';
 import { Provider } from 'react-redux';
 import Toast from 'react-native-toast-message';
-import { Foo } from './src/utils/toastHelper';
+import { Foo } from './src/utils/toast/toastHelper';
 
 function App() {
 
 
   return (
+  
     <>
     {/* <Provider store={store}>
       <StatusBar backgroundColor="#002335" barStyle="light-content" />
@@ -38,7 +39,9 @@ function App() {
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+  },
 });
 
 export default App;
